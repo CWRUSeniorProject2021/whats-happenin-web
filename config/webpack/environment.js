7b36b60f29e4e19ea24f5483d361a7b54e2dc939
Christian Tingle <ctingle18@gmail.com>
2021-09-05 17:40:16 -0400

    Updated environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,21 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack');
+const globImporter = require('node-sass-glob-importer');
+
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+
+environment
+  .loaders
+  .get('sass')
+  .use
+  .find(item => item.loader === 'sass-loader')
+  .options = { sassOptions: { importer: globImporter() } };
+
 module.exports = environment
