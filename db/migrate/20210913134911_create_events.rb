7a8b0617620fb6ecee46798a4c3a718f88a05fe4
Christian Tingle <ctingle18@gmail.com>
2021-09-16 20:47:11 -0400

    Removed index from migration

--- db/migrate/20210913134911_create_events.rb
+++ db/migrate/20210913134911_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
       t.string :title, null: false, default: "", limit: 255, index: true
-      t.text :description, null: false, default: "", limit: 10000, index: true
+      t.text :description, null: false, default: "", limit: 10000
       t.datetime :start_date, null: false, default: -> { 'CURRENT_TIMESTAMP' }, index: true
       t.datetime :end_date, null: false, default: -> { 'CURRENT_TIMESTAMP' }, index: true
       t.references :school, null: true, foreign_key: {on_delete: :restrict}, index: true
