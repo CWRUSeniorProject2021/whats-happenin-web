fc22746af996875a61e1d159c356a98243e7ae62
Anika Washburn <ajw228@cwru.edu>
2021-10-31 09:02:13 -0400

    add rsvp_type to event, clean white space up in controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,20 +3,18 @@ module Api
     class UsersController < ApplicationController
       before_action :authorize_user
 
-        def profile
-        end
-
+      def profile
+      end
 
     private
 
-        def authorize_user
-          case params[:action].to_sym
-          when :profile
-            @user = current_user
-          end
-          authorize @user || User
+      def authorize_user
+        case params[:action].to_sym
+        when :profile
+          @user = current_user
         end
-
+        authorize @user || User
+      end
     end
   end
 end
